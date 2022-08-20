851d75e0d0c5c52198a1dc03337084c80212aea3
Toby Roberts <tobyhazeroberts@gmail.com>
2022-08-20 17:57:28 -0600

    Added a top and bottom frame

--- Clothing Allowance App.py
+++ Clothing Allowance App.py
@@ -1,23 +1,39 @@
 from tkinter import *
 from tkinter import ttk
 
-
 # Create the window with title
 root=Tk()
 root.title("Clothing Allowance App")
 
+
+
+#Frame for top part of app
+top_frame = ttk.LabelFrame(root)
+top_frame.grid(row=0, column=0, padx=10, pady=10, sticky="NSEW")
+
+
+
 # Create and set the welcome text variable
 welcome_text = StringVar()
-welcome_text.set("Welcome! You can track and edit all your children's allowances and if they are on track to reach the goal. ")
+welcome_text.set("Welcome! You can track and edit all your children's allowances and if they are on track to reach the goal.")
 
 # Create and pack the welcome label
-welcome_label = Label(root, textvariable=welcome_text, wraplength=300)
+welcome_label = ttk.Label(top_frame, textvariable=welcome_text, wraplength=300)
 welcome_label.grid(row=0, column=0, columnspan=2, padx = 10, pady = 10)
 
-# Label for name combobox
-name_label = Label(root, text = "Who spent their allowance:")
-name_label.grid(row=3, column=0, padx = 10, pady = 10)
 
+
+
+# Frame for the bottom part of app
+bottom_frame = ttk.LabelFrame(root)
+bottom_frame.grid(row=1, column=0, padx=10, pady=10, sticky="NSEW")
+
+
+
+
+# Label for name combobox
+name_label = ttk.Label(bottom_frame, text = "Who spent their allowance:")
+name_label.grid(row=3, column=0, padx = 10, pady = 10)
 
 # Set up list and variable for combobox
 name_list = ["Nikau", "Hana", "Tia"]
@@ -25,13 +41,14 @@ chosen_name = StringVar()
 chosen_name.set(name_list[0])
 
 # Combobox to select name
-name_box = ttk.Combobox(textvariable=chosen_name, state="readonly")
+name_box = ttk.Combobox(bottom_frame, textvariable=chosen_name, state="readonly")
 name_box['values'] = name_list
 name_box.grid(row=3, column=1, padx = 10, pady = 10)
 
 
+
 # Label for the price entry
-price_label = Label(root, text = "Price of clothing:")
+price_label = ttk.Label(bottom_frame, text = "Price of clothing:")
 price_label.grid(row=4, column=0, padx = 10, pady = 10)
 
 # Variable to store the cost
@@ -39,8 +56,9 @@ price = DoubleVar()
 price.set("")
 
 # Entry for user to enter amoun
-price_entry = Entry(root, textvariable = price)
+price_entry = ttk.Entry(bottom_frame, textvariable = price)
 price_entry.grid(row=4, column=1, padx = 10, pady = 10)
+
 
 
 # Runs the main loop
