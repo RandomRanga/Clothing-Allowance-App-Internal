06e1db2dc52a395a1bbf9def03aecc3e9e71da59
Toby Roberts <tobyhazeroberts@gmail.com>
2022-08-20 17:17:58 -0600

    change .pack to .grid

--- Clothing Allowance App.py
+++ Clothing Allowance App.py
@@ -12,11 +12,11 @@ welcome_text.set("Welcome! You can deposit or withdraw money and see your progre
 
 # Create and pack the welcome label
 welcome_label = Label(root, textvariable=welcome_text, wraplength=300)
-welcome_label.pack()
+welcome_label.grid(row=0, column=0, columnspan=2, padx = 10, pady = 10)
 
 # Label for name combobox
 name_label = Label(root, text = "Who spent their allowance:")
-name_label.pack()
+name_label.grid(row=3, column=0, padx = 10, pady = 10)
 
 
 # Set up list and variable for combobox
@@ -27,12 +27,12 @@ chosen_name.set(name_list[0])
 # Combobox to select name
 name_box = ttk.Combobox(textvariable=chosen_name, state="readonly")
 name_box['values'] = name_list
-name_box.pack()
+name_box.grid(row=3, column=1, padx = 10, pady = 10)
 
 
 # Label for the price entry
 price_label = Label(root, text = "Price of clothing:")
-price_label.pack()
+price_label.grid(row=4, column=0, padx = 10, pady = 10)
 
 # Variable to store the cost
 price = DoubleVar()
@@ -40,7 +40,7 @@ price.set("")
 
 # Entry for user to enter amoun
 price_entry = Entry(root, textvariable = price)
-price_entry.pack()
+price_entry.grid(row=4, column=1, padx = 10, pady = 10)
 
 
 # Runs the main loop
