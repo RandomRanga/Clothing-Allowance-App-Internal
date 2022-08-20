7458f4432bf0be1d9a9c973e3b5c4a865c00cd95
Toby Roberts <tobyhazeroberts@gmail.com>
2022-08-20 11:26:00 -0600

    Price of Clothing entry

--- Clothing Allowance App.py
+++ Clothing Allowance App.py
@@ -2,7 +2,7 @@ from tkinter import *
 from tkinter import ttk
 
 
-# Creates the window with title
+# Create the window with title
 root=Tk()
 root.title("Clothing Allowance App")
 
@@ -13,6 +13,21 @@ welcome_text.set("Welcome! You can deposit or withdraw money and see your progre
 # Create and pack the welcome label
 welcome_label = Label(root, textvariable=welcome_text, wraplength=300)
 welcome_label.pack()
+
+# Label for the price entry
+price_label = Label(root, text="Price of clothing:")
+price_label.pack()
+
+# Variable to store the cost
+price = DoubleVar()
+price.set("")
+
+# Entry for user to enter amount
+price_entry = Entry(root, textvariable = price)
+price_entry.pack()
+
+
+
 
 
 
